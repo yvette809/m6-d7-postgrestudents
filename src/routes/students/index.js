22766a398a4f6bdc300a3fdb81dca17235d7668b
Yvette <nchombuayvta@gmil.com>
2020-07-14 20:31:23 +0200

    delete route working

--- src/routes/students/index.js
+++ src/routes/students/index.js
@@ -1,12 +1,21 @@
 const express = require("express")
 const db = require('../../db')
-
+const students = require ('./students.json')
 const studRouter = express.Router()
+
+//import students from students.json and upload in to our database
+
+studRouter.post('/import', async(req,res)=>{
+
+})
+
 
 studRouter.get('/', async (req,res)=>{
     const response = await db.query('SELECT * FROM "students"')
     res.send(response.rows)
 })
+
+// get a single student
 
 studRouter.get("/:_id", async (req,res)=>{
     const response = await db.query('SELECT _id, name,surname,email,dateOfBirth FROM "students" WHERE _id = $1',[req.params._id])
@@ -17,6 +26,27 @@ studRouter.get("/:_id", async (req,res)=>{
     }
 })
 
+// create student
+studRouter.post("/", async(req,res)=>{
+    const response = await db.query(`INSERT INTO "students"(_id,name,surname,email,dateOfBirth)
+                                                            values($1,$2,$3,$4,$5)
+                                                             RETURNING *`,
+                                                              [req.body._id,req.body.name,req.body.surname,req.body.email,req.body.dateOfBirth])
+        console.log(response)
+        res.send(response.rows[0])                                                      
+})
+
+// update a student
+
+// delete a student
+studRouter.delete("/", async(req,res)=>{
+    const response = await db.query(`DELETE FROM "students" WHERE _id = $1`,[req.params._id] )
+    if(response.rowCount===0){
+    return res.status(404).send("Not Found")
+    }else{
+        res.send('Deleted')
+    }
+})
 
 
 module.exports = studRouter;
