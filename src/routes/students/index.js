5c863a9e09802b5e6adaf1e6c4b667b724896f9a
Yvette <nchombuayvta@gmil.com>
2020-07-14 21:39:23 +0200

    students imported

--- src/routes/students/index.js
+++ src/routes/students/index.js
@@ -6,7 +6,27 @@ const studRouter = express.Router()
 //import students from students.json and upload in to our database
 
 studRouter.post('/import', async(req,res)=>{
+    const response = await db.query('SELECT _id FROM "students"')
+    const idList = response.rows.map(x=> x._id)
 
+    let total = 0
+    let skipped = 0
+    students.forEach(async stud=>{
+        if (idList.indexOf(stud._id) === -1){
+        await db.query(`INSERT INTO "students" (_id,name,surname,email,dateOfBirth)
+        values($1, $2,$3,$4,$5)`,
+        [stud._id, stud.name,stud.surname, stud.email,stud.dateOfBirth])
+        total++
+        
+        } else{
+            console.log(`Element ${stud._id} is already in the DB!`)
+            skipped ++
+        }  
+    })
+        res.send({
+            added:total,
+            skipped
+        })
 })
 
 
@@ -37,6 +57,22 @@ studRouter.post("/", async(req,res)=>{
 })
 
 // update a student
+studRouter.put('/:_id', async(req,res)=>{
+    try{
+        const result = await db.query(`UPDATE "students"
+                                  SET name = $1,
+                                  surname = $2,
+                                  email = $3,
+                                  dateOfBirth = $4
+                                  WHERE _id = $2`,
+                                  [req.body.name,req.body.surname,req.body.email,req.body.dateOfBirth,req.params._id])
+        if(result.rowCount===0)
+            return res.status(404).send('not found')
+        res.send(result.rows[0])
+    }catch(ex){
+        res.status(500).send(ex)
+    }
+})
 
 // delete a student
 studRouter.delete("/", async(req,res)=>{
@@ -49,4 +85,4 @@ studRouter.delete("/", async(req,res)=>{
 })
 
 
-module.exports = studRouter;
+module.exports= studRouter
