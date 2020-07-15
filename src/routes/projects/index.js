c56a5c7de1064111171beddf3c7ed4f5d0b4939c
Yvette <nchombuayvta@gmil.com>
2020-07-15 19:00:59 +0200

    single project got

--- src/routes/projects/index.js
+++ src/routes/projects/index.js
@@ -3,12 +3,24 @@ const db = require('../../db')
 const projects = require ('./projects.json')
 
 const projRouter = express.Router()
-
+// get projects
 projRouter.get("/", async (req,res)=>{
     console.log("database")
     const response = await db.query('SELECT * FROM "projects"')
    
     res.send(response.rows)
+})
+
+// get a single project 
+projRouter.get("/:_id", async(req,res)=>{
+    const response = await db.query(`SELECT _id,name,description,creationDate,repoURL,liveURL,studentID FROM "projects" WHERE _id = $1`,
+                                                                                                         [req.params._id])
+    if (response.rowCount===0){
+        return res.status(404).send('not found')
+    }else{
+       return res.status(200).send(response.rows[0])
+    }
+
 })
 
 
